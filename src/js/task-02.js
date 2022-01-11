04f47bb7de81837e10256de7609f54679c9fe493
Yuliia-Rozhnova <89421539+Yuliia-Rozhnova@users.noreply.github.com>
2022-01-12 01:43:45 +0200

    задача 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+
+const itemsIngredients = ingredients.map((ingredient) => {
+ const itemsLi = document.createElement('li');
+  itemsLi.textContent = ingredient;
+  itemsLi.classList.add('item');
+  return itemsLi;
+
+});
+
+ingredientsRef.append(...itemsIngredients);
+console.log(itemsIngredients);
