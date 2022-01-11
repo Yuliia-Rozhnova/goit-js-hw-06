a749fea9df6fbc7cd7205ed9636705a2ad5cec8e
Yuliia-Rozhnova <89421539+Yuliia-Rozhnova@users.noreply.github.com>
2022-01-12 00:36:45 +0200

    дз

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+const categoriesEl = document.querySelectorAll('#categories');
+console.log(categoriesEl);
+const elements = document.querySelectorAll('.item');
+console.log(elements);
+const titleEl = document.querySelector('h2');
+console.log(titleEl.textContent);
+
+
+
+
+
+
+
+
