627efc7cd334f0dd19738426a3f69f5701e5f3bb
Yuliia-Rozhnova <89421539+Yuliia-Rozhnova@users.noreply.github.com>
2022-02-18 23:43:35 +0200

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,6 @@
+const btnRef = document.querySelectorAll('#counter');
+console.log(btnRef);
+const decrementBtn = document.querySelector('[data-action]');
+const incrementBtn = document.querySelector('[data-action]');
+
+const counter
